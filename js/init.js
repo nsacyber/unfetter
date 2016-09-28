8fa91d9d0cf0b25188e0bf85b3ee4c00902d0edb
Alchemist <infosec.alchemist@gmail.com>
2016-09-28 11:54:41 -0400

    Updated the webpage to remove the wiki information and move it to the main webpage

--- js/init.js
+++ js/init.js
@@ -1,7 +1,17 @@
-(function($){
-  $(function(){
 
-    $('.button-collapse').sideNav();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function(){
+      
+      // Sidebar
+      $(".button-collapse").sideNav({menuWidth: 320, activationWidth: 70, edge: 'left'});
+      // Dropdown
+      $('.dropdown-button').dropdown({
+           inDuration: 300,
+           outDuration: 225,
+           constrain_width: false, // Does not change width of dropdown to that of the activator
+           hover: false, // Activate on hover
+           gutter: 0, // Spacing from edge
+           belowOrigin: false // Displays dropdown below the button
+           }
+      );
+    });
